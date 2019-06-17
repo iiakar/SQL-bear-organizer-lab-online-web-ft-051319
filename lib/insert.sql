f044c4c5e9a68346832798c10c7b31aec6090a30
Ibrahim  Akar <ibsakar@gmail.com>
2019-06-17 15:09:39 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -6,4 +6,4 @@ INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
 ('Melissa', 13, 'F', 'dark brown', 'goofy', 1),
 ('Grinch', 2, 'M', 'Black', 'Grinchy', 1),
 ('Wendy', 6, 'F', 'Blue', 'naive', 1),
-(null, 20, 'M', 'black', 'aggressive', 0);
+(NULL, 20, 'M', 'black', 'aggressive', 0);
