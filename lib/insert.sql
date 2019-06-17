7f3af7038cb9c4bee0c462dd3bac3a8beb5303e5
Ibrahim  Akar <ibsakar@gmail.com>
2019-06-17 15:07:29 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,6 +1,6 @@
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
-("Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
-("Rowdy", 10, "M", "black", "intense", 1),
+('Mr. Chocolate', 20, 'M', 'dark brown', 'calm', 0),
+('Rowdy", 10, "M", "black", "intense", 1),
 ("Tabitha", 6, "F", "dark brown", "Nice", 1),
 ("Sargeant Brown", 19, "M", "Green", "Slimy", 0),
 ("Melissa", 13, "F", "dark brown", "goofy", 1),
